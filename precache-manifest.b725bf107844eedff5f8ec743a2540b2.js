cec77f615c8a24b3eeae149f2ae04b030e77947f
Taariq Levack <levackt@users.noreply.github.com>
2020-08-21 17:01:25 +0200

    Updates

---
+++ precache-manifest.b725bf107844eedff5f8ec743a2540b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c895436aea878f546ab0c258d1ee97d",
+    "url": "/sample-sapp/index.html"
+  },
+  {
+    "revision": "22ec43743f263bcbbb60",
+    "url": "/sample-sapp/static/css/main.d63753fc.chunk.css"
+  },
+  {
+    "revision": "361d5e1cf8655a681671",
+    "url": "/sample-sapp/static/js/2.37710cbb.chunk.js"
+  },
+  {
+    "revision": "b93ece77fcbdc2e55817f33239e857b8",
+    "url": "/sample-sapp/static/js/2.37710cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ec43743f263bcbbb60",
+    "url": "/sample-sapp/static/js/main.29c67e63.chunk.js"
+  },
+  {
+    "revision": "916622799514b488e888",
+    "url": "/sample-sapp/static/js/runtime-main.1b220969.js"
+  }
+]);
